91aeb0212a825510aca3fa4216af1cc28d0e12bc
Sudip-Thakur <sudipthakur643@gmail.com>
2024-08-10 09:00:58 +0545

    change

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Comment from './Comment.jsx' // Adjust the import path as needed
 
 const CommentsSection = (comments) => {
-
+  console.log(comments)
   return (
     <div className="max-w-4xl mx-auto p-4">
       <h2 className="text-xl font-bold mb-4">Comments</h2>
