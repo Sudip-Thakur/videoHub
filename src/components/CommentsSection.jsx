b64fc57966524c41bf990b62a472a502845ffdb0
Sudip-Thakur <sudipthakur643@gmail.com>
2024-08-10 08:57:48 +0545

    change

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -6,14 +6,7 @@ const CommentsSection = (comments) => {
   return (
     <div className="max-w-4xl mx-auto p-4">
       <h2 className="text-xl font-bold mb-4">Comments</h2>
-      {comments.map((comment, index) => (
-          <Comment
-            // key={index}
-            // userAvatar={comment.avatar}
-            // userName={comment.username}
-            // commentContent={comment.content}
-          />
-        ))}
+      <Comment/>
     </div>
   );
 };
